dd96d547ebe1f0fbfca7aea5b75b0674957ab672
KrieqqeR <burakcevizli@gmail.com>
2023-08-10 18:50:15 +0300

    Cypress Çalışıldı

---
+++ cypress/e2e/form.cy.js
@@ -0,0 +1,27 @@
+describe("Ödev TEST", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    it("Name Check", () => {
+        cy.get("#name").type("Burak").should("have.value", "Burak")
+    });
+    it("Email Check", () => {
+        cy.get('#email').type("burakcevizli@gmail.com").should("have.value", "burakcevizli@gmail.com")
+    })
+    it("Password Check", () => {
+        cy.get('#password').type("2511606").should("have.value", "2511606")
+    })
+    it("TC Check", () => {
+        cy.get('#tcKimlik').type("20267367104").should("have.value", "20267367104")
+    })
+    it("Rol Check", () => {
+        cy.get('select').select('Mechanic').should('have.value', 'role 3')
+    })
+    it("Terms Check", () => {
+        cy.get('#terms').check()
+    })
+    it("Buton Check", () => {
+        cy.get('.btn').click()
+    })
+})
