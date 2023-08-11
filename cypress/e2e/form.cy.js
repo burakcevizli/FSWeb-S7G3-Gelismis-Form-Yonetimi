fd01ffe73076a5124e235bd0f9c7616a143d43ba
KrieqqeR <burakcevizli@gmail.com>
2023-08-11 09:53:02 +0300

    güncel form

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -3,25 +3,25 @@ describe("Ödev TEST", () => {
         cy.visit("http://localhost:3000");
     });
 
-    it("Name Check", () => {
+    it("Girilen Veriler Görülüyor mu state ve view baglı mı", () => {
+        // cy.get('.btn').should('be.disabled')
         cy.get("[data-cy=isimformu]").type("Burak").should("have.value", "Burak")
-    });
-    it("Email Check", () => {
         cy.get('#email').type("burakcevizli@gmail.com").should("have.value", "burakcevizli@gmail.com")
-    })
-    it("Password Check", () => {
         cy.get('#password').type("2511606").should("have.value", "2511606")
-    })
-    it("TC Check", () => {
         cy.get('#tcKimlik').type("20267367104").should("have.value", "20267367104")
-    })
-    it("Rol Check", () => {
         cy.get('select').select('Mechanic').should('have.value', 'role 3')
+        cy.get('#terms').check().should('be.checked')
+        // cy.get('.btn').should('be.enabled')
     })
-    it("Terms Check", () => {
-        cy.get('#terms').check()
-    })
-    it("Buton Check", () => {
-        cy.get('.btn').click()
+
+    it("Hatalı giriş yaptıgımda hata gözüküyor mu", () => {
+        // cy.get('.btn').should('be.disabled')
+        cy.get("[data-cy=isimformu]").type("Burak").should("have.value", "Burak")
+        cy.get('#email').type("burakcevizli@gmail.com").should("have.value", "burakcevizli@gmail.com")
+        cy.get('#password').type("2511606").should("have.value", "2511606")
+        cy.get('#tcKimlik').type("20267367104").should("have.value", "20267367104")
+        cy.get('select').select('Mechanic').should('have.value', 'role 3')
+        cy.get('#terms').check().should('be.checked')
+        // cy.get('.btn').should('be.enabled')
     })
 })
